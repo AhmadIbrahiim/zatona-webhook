5bc51a4773e972ce29c98c39c3fa131701705197
Ahmed Ibrahim <Ahmed Ibrahim>
2017-04-25 21:56:29 +0200

    make it better

--- temp.js
+++ temp.js
@@ -58,7 +58,7 @@ function Getproducts(callback) {
 	                "title":"عرض التفاصيل",
 	                "payload":"showdetails"
 	              }              
-	            ] ;
+	            ] 
 	        	}
 	        ]	
 			
