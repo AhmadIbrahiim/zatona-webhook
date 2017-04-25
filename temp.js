c5a13ce2f03b0b374a91fe29014747aea89eff4d
Ahmed Ibrahim <Ahmed Ibrahim>
2017-04-25 21:38:38 +0200

    make it better

---
+++ temp.js
@@ -0,0 +1,79 @@
+function Getproducts(callback) {
+
+	var Ob =
+		{
+	    "attachment":{
+	      "type":"template",
+	      "payload":{
+	        "template_type":"generic",
+	        "elements":[
+	           {
+	            "title":"Welcome to Peter\'s Hats",
+	            "image_url":"https://petersfancybrownhats.com/company_image.png",
+	            "subtitle":"We\'ve got the right hat for everyone.",
+	            "buttons":[
+	          	{
+	                "type":"postback",
+	                "title":"عرض التفاصيل",
+	                "payload":"showdetails"
+	              }              
+	            ]      
+	          },
+	          {
+	            "title":"Welcome to Peter\'s Hats",
+	            "image_url":"https://petersfancybrownhats.com/company_image.png",
+	            "subtitle":"We\'ve got the right hat for everyone.",
+	            "buttons":[
+	             {
+	                "type":"postback",
+	                "title":"عرض التفاصيل",
+	                "payload":"showdetails"
+	              }              
+	            ]      
+	          },{
+	            "title":"Welcome to Peter\'s Hats",
+	            "image_url":"https://petersfancybrownhats.com/company_image.png",
+	            "subtitle":"We\'ve got the right hat for everyone.",
+	            
+	            "buttons":[
+	            {
+	                "type":"postback",
+	                "title":"عرض التفاصيل",
+	                "payload":"showdetails"
+	              }              
+	            ]      
+	          },{
+	            "title":"Welcome to Peter\'s Hats",
+	            "image_url":"https://petersfancybrownhats.com/company_image.png",
+	            "subtitle":"We\'ve got the right hat for everyone.",
+	            "buttons":[
+	             ,{
+	                "type":"postback",
+	                "title":"عرض التفاصيل",
+	                "payload":"showdetails"
+	              }              
+	            ]      
+	          },{
+	            "title":"Welcome to Peter\'s Hats",
+	            "image_url":"https://petersfancybrownhats.com/company_image.png",
+	            "subtitle":"We\'ve got the right hat for everyone.",
+	            "buttons":[
+	             ,{
+	                "type":"postback",
+	                "title":"عرض التفاصيل",
+	                "payload":"showdetails"
+	              }              
+	            ]      
+	          }
+	        ]
+	      }
+	    }
+	  }
+	
+
+
+
+
+module.exports=Getproducts;
+
+
