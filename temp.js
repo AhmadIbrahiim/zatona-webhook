3be4182ded4a54af0cb7bf4974f0e08e12f19a3c
Ahmed Ibrahim <Ahmed Ibrahim>
2017-04-25 22:05:53 +0200

    make it better

--- temp.js
+++ temp.js
@@ -1,8 +1,7 @@
 
 function Getproducts(callback) {
-
-	var Ob =[
-	           {
+	var Ob = []
+	Ob.push({
 	            "title":"Welcome to Peter\'s Hats",
 	            "image_url":"https://petersfancybrownhats.com/company_image.png",
 	            "subtitle":"We\'ve got the right hat for everyone.",
@@ -13,55 +12,56 @@ function Getproducts(callback) {
 	                "payload":"showdetails"
 	              }              
 	            ]      
-	          },
-	          {
+	          });
+	Ob.push({
 	            "title":"Welcome to Peter\'s Hats",
 	            "image_url":"https://petersfancybrownhats.com/company_image.png",
 	            "subtitle":"We\'ve got the right hat for everyone.",
 	            "buttons":[
-	             {
+	          	{
 	                "type":"postback",
 	                "title":"عرض التفاصيل",
 	                "payload":"showdetails"
 	              }              
 	            ]      
-	          },{
+	          });
+	Ob.push({
 	            "title":"Welcome to Peter\'s Hats",
 	            "image_url":"https://petersfancybrownhats.com/company_image.png",
 	            "subtitle":"We\'ve got the right hat for everyone.",
-	            
 	            "buttons":[
-	            {
+	          	{
 	                "type":"postback",
 	                "title":"عرض التفاصيل",
 	                "payload":"showdetails"
 	              }              
 	            ]      
-	          },{
+	          });
+	Ob.push({
 	            "title":"Welcome to Peter\'s Hats",
 	            "image_url":"https://petersfancybrownhats.com/company_image.png",
 	            "subtitle":"We\'ve got the right hat for everyone.",
 	            "buttons":[
-	             ,{
+	          	{
 	                "type":"postback",
 	                "title":"عرض التفاصيل",
 	                "payload":"showdetails"
 	              }              
 	            ]      
-	          },{
+	          });
+	Ob.push({
 	            "title":"Welcome to Peter\'s Hats",
 	            "image_url":"https://petersfancybrownhats.com/company_image.png",
 	            "subtitle":"We\'ve got the right hat for everyone.",
 	            "buttons":[
-	             ,{
+	          	{
 	                "type":"postback",
 	                "title":"عرض التفاصيل",
 	                "payload":"showdetails"
 	              }              
-	            ] 
-	        	}
-	        ]	
-			
+	            ]      
+	          });
+	
 
 	  callback(Ob);
 	}
